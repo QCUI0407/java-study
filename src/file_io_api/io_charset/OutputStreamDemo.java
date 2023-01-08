1308c4821f69d3985c70e4a8ac7e20a4ae4f9fa5
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-01-07 23:21:38 -0600

    IO, path

--- src/file_io_api/io_charset/OutputStreamDemo.java
+++ src/file_io_api/io_charset/OutputStreamDemo.java
@@ -8,8 +8,16 @@ import java.io.OutputStream;
 
 public class OutputStreamDemo {
     public static void main(String[] args) throws Exception {
+        String filePath = "C:\\Users\\cuiqi\\OneDrive\\Desktop\\java-study\\src\\out.txt";
+        File file = new File(filePath);
+        System.out.println("---------"+file);
+
+        System.out.println(file.isFile());
+
+        System.out.println(file.exists());
+
         //1.创建一个文件文件输出管道与目标文件接通
-        OutputStream os = new FileOutputStream("JAVA-STUDY/src/out.txt");
+        OutputStream os = new FileOutputStream(file);
         //2.写数据
         os.write('c');
         os.write('u');
