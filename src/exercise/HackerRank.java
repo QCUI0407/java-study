61b2e457166f3ffda746deb0d20e2bcc3827771e
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-01-17 22:20:08 -0600

    SQL

---
+++ src/exercise/HackerRank.java
@@ -0,0 +1,15 @@
+package exercise;
+import java.util.List;
+public class HackerRank {
+    public static void main(String[] args) {
+        
+    }
+    /*
+     * 
+     */
+    public static int hourglassSum(List<List<Integer>> arr) {
+        return 0;
+        // Write your code here
+    
+        }
+}
